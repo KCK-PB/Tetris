10d5e3eb7fbaa8d6287b8c3d8c04714f636be1af
Sylwia0506 <sysazeszkoly@gmail.com>
2021-10-31 15:26:37 +0100

    Added csproj file for the Tetris project, removed all old files.

---
+++ TetrisMain/Models/Square.cs
@@ -0,0 +1,40 @@
+﻿namespace TetrisMain.Models
+{
+    using System;
+    
+    public class Square
+    {
+        private int _posx;
+        private int _posy;
+
+        public Square(int posx, int posy)
+        {
+            this._posx = posx;
+            this._posy = posy;
+        }
+
+        public Tuple<int, int> GetPos()
+        {
+            return new Tuple<int, int>(_posx, _posy);
+        }
+
+        public void MovePos(string direction)
+        {
+            //bottom=table[0][x] left=table[x][0] right=table[x][9] top=table[23][x]
+            switch (direction)
+            {
+                case "left":
+                    _posy--;
+                    break;
+
+                case "right":
+                    _posy++;
+                    break;
+
+                case "down":
+                    _posx--;
+                    break;
+            }
+        }
+    }
+}
---
+++ TetrisMain/Models/TetrisPlayboard.cs
@@ -0,0 +1,59 @@
+﻿namespace TetrisMain.Models
+{
+    public class TetrisPlayboard
+    {
+        private readonly char[,] _playboard;
+        private readonly char[,] _drawboard;
+
+        private readonly TetrisBlock
+            _currentPiece =
+                new("J-block"); //placeholder current piece, TO-DO: add method to create current piece when last was placed on playboard
+
+        private static TetrisPlayboard instance = null;
+
+        private TetrisPlayboard()
+        {
+            _playboard = new char[24, 10];
+            _drawboard = new char[24, 10];
+            
+            for (var i = 0; i < 24; i++)
+                for (var j = 0; j < 10; j++)
+                {
+                    _playboard[i, j] = '.';
+                    _drawboard[i, j] = ' ';
+                }
+        }
+
+        public static TetrisPlayboard GetInstance()
+        {
+            if (instance == null) instance = new TetrisPlayboard();
+            
+            return instance;
+        }
+
+        public bool IsOccupied(int posx, int posy)
+        {
+            if (_playboard[posx, posy] != ' ')
+                return true;
+            return false;
+        }
+
+        public void DrawBoard()
+        {
+            for (var i = 0; i < 24; i++)
+                for (var j = 0; j < 10; j++)
+                    _drawboard[i, j] = _playboard[i, j];
+            
+            var tempPosition = _currentPiece.GetPosition();
+            
+            for (var i = 0; i < 4; i++)
+                _drawboard[tempPosition[i].GetPos().Item1, tempPosition[i].GetPos().Item2] = 'X';
+        }
+
+        public void MoveTetrisBlock()
+        {
+            //TO-DO add collisions and block placement at the bottom of playboard
+            _currentPiece.MoveTetrisBlock("down");
+        }
+    }
+}
