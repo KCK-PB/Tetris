cb992d25b577d559be706a8c966400945695e5e4
Sylwia0506 <sysazeszkoly@gmail.com>
2021-11-06 14:53:10 +0100

    Add Bye in Exit()

--- TetrisMain/Program.cs
+++ TetrisMain/Program.cs
@@ -10,6 +10,66 @@ using TetrisMain.Models;
 namespace TetrisMain {
 
     class Program {
+
+        static void ConsoleDraw(IEnumerable<string> lines, int x, int y)
+        {
+            if (x > Console.WindowWidth) return;
+            if (y > Console.WindowHeight) return;
+
+            var trimLeft = x < 0 ? -x : 0;
+            int index = y;
+
+            x = x < 0 ? 0 : x;
+            y = y < 0 ? 0 : y;
+
+            var linesToPrint =
+                from line in lines
+                let currentIndex = index++
+                where currentIndex > 0 && currentIndex < Console.WindowHeight
+                select new
+                {
+                    Text = new String(line.Skip(trimLeft).Take(Math.Min(Console.WindowWidth - x, line.Length - trimLeft)).ToArray()),
+                    X = x,
+                    Y = y++
+                };
+
+            Console.Clear();
+            foreach (var line in linesToPrint)
+            {
+                Console.SetCursorPosition(line.X, line.Y);
+                Console.Write(line.Text);
+            }
+        }
+
+        static void Exit()
+        {
+            Console.CursorVisible = false;
+
+            var arr = new[]
+            {
+@"   _____                        ",
+@"|       \                      ",
+@"| $$$$$$$\ __    __   ______   ",
+@"| $$__/ $$|  \  |  \ /      \  ",
+@"| $$    $$| $$  | $$|  $$$$$$\ ",
+@"| $$$$$$$\| $$  | $$| $$    $$ ",
+@"| $$__/ $$| $$__/ $$| $$$$$$$$ ",
+@"| $$    $$ \$$    $$ \$$     \ ",
+@" \$$$$$$$  _\$$$$$$$  \$$$$$$$ ",
+@"           |  \__| $$          ",
+@"           \$$    $$           ",
+@"            \$$$$$$            ",
+       };
+
+            var maxLength = arr.Aggregate(0, (max, line) => Math.Max(max, line.Length));
+            var x = Console.BufferWidth / 2 - maxLength / 2;
+            for (int y = -arr.Length; y < Console.WindowHeight + arr.Length; y++)
+            {
+                ConsoleDraw(arr, x, y);
+                Thread.Sleep(100);
+            }
+            Environment.Exit(0);
+        }
         public static List<Option> options;
         static void Main(string[] args) {
             ConsoleHelper.SetCurrentFont("Terminal",32);
@@ -28,7 +88,7 @@ namespace TetrisMain {
                 new Option("Start Game", () => PrepareGame()),
                 new Option("Continue", () =>  WriteTemporaryMessage("Continue")),
                 new Option("Scoreboard", () =>  WriteTemporaryMessage("Scoreboard")),
-                new Option("Exit", () => Environment.Exit(0)),
+                new Option("Exit", () => Exit()),
             };
             int index = 0;
 
